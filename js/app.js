e7381d6fbd74907ba228a204e33ef06376632563
Yesenia <yeseniasescarcega@gmail.com>
2017-05-03 13:11:19 -0500

    Se agrega funcionalidad del modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+var modal = document.getElementById("fondoModal");
+var img = document.getElementsByClassName("img");
+var imgProducto = document.getElementById("img01");
+var borrar = document.getElementsByClassName("cerrar")[0];
 
- console.log("Hola");
+borrar.addEventListener("click",cerrar);
+
+for (var i = 0; i < img.length; i++) {
+  img[i].addEventListener("click",popUp);
+}
+
+function popUp() {
+  modal.style.display = "block";
+  imgProducto.src = this.src;
+}
+
+function cerrar() {
+  modal.style.display = "none";
+
+}
